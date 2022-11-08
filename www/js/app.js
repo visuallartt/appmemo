5981c6429deca8baa30dc7094d890c68f2df20a4
Chloe Deschamps <deschamps-chloe@orange.fr>
2022-11-08 10:29:11 +0100

    ajout timer

--- www/js/app.js
+++ www/js/app.js
@@ -43,3 +43,20 @@ function match(cardOne, cardTwo) {
         }, 500)
     }
 }
+
+//timer
+const departMinutes = 1
+        let temps = departMinutes * 60
+
+        const timerElement = document. getElementById("time")
+
+        setInterval(() => {
+        let minutes = parseInt(temps / 60, 10)
+        let secondes = parseInt(temps % 60, 10)
+
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        secondes = secondes < 10 ? "0" + secondes : secondes
+
+        timerElement.innerText = `${minutes}:${secondes}`
+        temps = temps <= 0 ? 0 : temps - 1
+        }, 1000)
