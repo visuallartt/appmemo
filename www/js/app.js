7d1e7bcaf73e391aa3babfdb6f516d3715971372
Noah ABRAHAM <noahabraham214@gmail.com>
2022-11-08 09:44:18 +0100

    Ajout d'un timer

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,11 @@
 
+/*Cards*/
 const card = document.querySelectorAll(".card")
 
 randomCard()
 clickCard()
 
+/*Création aléatoire cartes*/
 function randomCard() {
 
     card.forEach(e => {
@@ -14,6 +16,7 @@ function randomCard() {
 
 }
 
+/*Comportement cards au clique dessus*/
 function clickCard() {
 
     for (let i = 0; i < card.length; i++) {
@@ -29,6 +32,7 @@ function clickCard() {
     }
 }
 
+/*Comportement jeu lorsque les cartes correspondent ou non*/
 function match(cardOne, cardTwo) {
     if (cardOne.dataset.index === cardTwo.dataset.index) {
         cardOne.classList.remove('flip')
@@ -43,3 +47,70 @@ function match(cardOne, cardTwo) {
         }, 500)
     }
 }
+
+/*Timer*/
+class Timer {
+    constructor() {
+        this.isRunning = false;
+        this.startTime = 0;
+        this.overallTime = 0;
+    }
+
+    _getTimeElapsedSinceLastStart() {
+        if (!this.startTime) {
+            return 0;
+        }
+
+        return Date.now() - this.startTime;
+    }
+
+    start() {
+        if (this.isRunning) {
+            return console.error('Timer is already running');
+        }
+
+        this.isRunning = true;
+
+        this.startTime = Date.now();
+    }
+
+    stop() {
+        if (!this.isRunning) {
+            return console.error('Timer is already stopped');
+        }
+
+        this.isRunning = false;
+
+        this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+
+    reset() {
+        this.overallTime = 0;
+
+        if (this.isRunning) {
+            this.startTime = Date.now();
+            return;
+        }
+
+        this.startTime = 0;
+    }
+
+    getTime() {
+        if (!this.startTime) {
+            return 0;
+        }
+
+        if (this.isRunning) {
+            return this.overallTime + this._getTimeElapsedSinceLastStart();
+        }
+
+        return this.overallTime;
+    }
+}
+
+const timer = new Timer();
+timer.start();
+setInterval(() => {
+    const timeInSeconds = Math.round(timer.getTime() / 1000);
+    document.getElementsByClassName('time').innerText = timeInSeconds;
+}, 100)
